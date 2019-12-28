0f2d4a46f9c335c4e1d51a2f0ec2f508f8bf0c65
Rodrigo Muniz <rodrigosilvamuniz@gmail.com>
2019-12-28 00:36:02 -0300

    Initial Modifications on main.py for Input File Reading
    Initial modifications on main.py for input file reading.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from sys import argv
 from os.path import abspath
 from configuration.config import Configuration
 from argument.arg import Argument
+from input.input_txt import define_input_dict
 
 
 def main():
@@ -11,6 +12,11 @@ def main():
 
     if len(argv) > 1:
         args = cmd.parser.parse_args()
+
+        with open(args.inp, "r", encoding="utf-8-sig") as input_file:
+            feeder_dict = define_input_dict(input_file=input_file)
+
+        print(feeder_dict)
     else:
         args = cmd.parser.parse_args(["-h"])
 

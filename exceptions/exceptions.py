87deee29044d94cf729cdacadb4e519fd33ff29c
Rodrigo Muniz <rodrigosilvamuniz@gmail.com>
2019-12-30 14:20:57 -0300

    Adding Exceptions for Handling Input Errors
    Added exceptions for error handling in input files.

--- exceptions/exceptions.py
+++ exceptions/exceptions.py
@@ -2,3 +2,27 @@ class InputValueError(ValueError):
     def __init__(self, message, errors=None):
         super().__init__(message)
         self.errors = errors
+
+
+class DictValueError(ValueError):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+
+
+class EmptyRequiredArgument(Exception):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+
+
+class IncorrectInputFormat(ValueError):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
+
+
+class IncorrectDictFormat(ValueError):
+    def __init__(self, message, errors=None):
+        super().__init__(message)
+        self.errors = errors
